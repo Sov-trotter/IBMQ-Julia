96f3bbe619bddfa4ce9788904293bf6b9b698016
ARSH SHARMA <184517@nith.ac.in>
2020-10-15 16:33:28 +0530

    Finzalize stuff

--- init.jl
+++ init.jl
@@ -10,13 +10,27 @@ struct Qobj
     data::Dict{String, Any}
 end
 
-struct IBMQReg 
+struct IBMQReg{B} <: AbstractRegister{B}
     id::String
     device::String
-    uploadurl::String
-    jobid::String     
+    nactive::B
+    nqubits::B
 end
 
+struct Job
+    reg::IBMQReg
+    data::Qobj
+    jobid::String
+end
+
+Yao.nactive(reg::IBMQReg) = reg.nactive
+Yao.nqubits(reg::IBMQReg) = reg.nqubits
+# Yao.device(reg::IBMQReg) = reg.device
+
+function apply!(reg::IBMQReg, qc::Array{ChainBlock{N}}) where N
+    qobj = yaotoqobj(qc, reg.device)
+    job = run(reg, qobj)
+end
 """
 Login method
 """
@@ -32,12 +46,15 @@ function authenticate(token::String = "") # todo : save the token
 
     url = "https://api.quantum-computing.ibm.com/api/users/loginWithToken"
     req = Dict("apiToken" => token)
+    print("Logging You in...")
     response = HTTP.post(url, headers, JSON.json(req); conf...)
     
     response_json = String(response.body)
     response_parsed = JSON.parse(response_json)
     if response.status == 200
-        println("Login Successful")
+        println("✔")
+    else
+        println("❌")
     end
     IBMQUser(response_parsed["id"])
 end
@@ -48,18 +65,44 @@ Get backends info
 function get_backends(user::IBMQUser)
     id = user.id
     url_back = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/devices/v/1?access_token=$(id)"
+    println("Fetching Backends...")
     response_back = HTTP.get(url_back)
     response_back_json = String(response_back.body)
     response_back_parsed = JSON.parse(response_back_json)
-    device_info = response_back_parsed[1]
     id_back = [i["backend_name"] for i in response_back_parsed]
+    confirm = false
+    while !confirm
+        println("The following backends are available > ")
+        for i in enumerate(id_back)
+            println(i)
+        end
+        println("Enter the serial number of the backend you wish to use")
+        backend_no = readline()
+        device_info = response_back_parsed[parse(Int64, backend_no)]
+        n_qubits=device_info["n_qubits"]
+        println("n_qubits = $(n_qubits)")
+        println("basis_gates=$(device_info["basis_gates"])")
+        println("Confirm? (Y/N)")
+        conf = readline()
+        if conf == "Y" || conf == "y"
+            return IBMQReg(id, id_back[parse(Int, backend_no)], n_qubits, n_qubits)
+        else
+            confirm = false
+        end
+    end
 end
 
-function run(user::IBMQUser, qobj::Qobj, device::String)
-    url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs?access_token=$(id)" #user.id
+function run(reg::IBMQReg, qobj::Qobj)
+    url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs?access_token=$(reg.id)" 
 
-    req = Dict("backend" => Dict("name" => device), "allowObjectStorage" => true, "shareLevel"=> "none")
+    req = Dict("backend" => Dict("name" => reg.device), "allowObjectStorage" => true, "shareLevel"=> "none")
+    print("Connecting to $(reg.device)...")
     request = HTTP.post(url , headers, JSON.json(req))
+    if request.status == 200
+        println("✔")
+    else
+        println("❌")
+    end
     response_json = String(request.body)
     response_parsed = JSON.parse(response_json)
     
@@ -67,21 +110,32 @@ function run(user::IBMQUser, qobj::Qobj, device::String)
     upload_url = objectinfo["uploadUrl"]
     jobid = response_parsed["id"]
 
+    print("Preparing Data...")
     json = JSON.json(qobj.data)
-    ckt_upload = HTTP.put(upload_url, [], json) 
+    println("✔")
 
-    #if 200
-    # Notify the backend that the job has been uploaded
-    url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)/jobDataUploaded?access_token=$(id)"
-    json_step4 ="""{
+    print("Uploading circuit to $(reg.device)...")
+    ckt_upload = HTTP.put(upload_url, [], json) 
+    if ckt_upload.status == 200
+        println("✔")
+        print("Notifying backend....")
+        # Notify the backend that the job has been uploaded
+        url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)/jobDataUploaded?access_token=$(reg.id)"
+        json_step4 ="""{
                 "data": "none",
                 "json": "none"
-            }"""
-    request = HTTP.post(url, headers, json_step4)
-    return job_id
+                }"""
+        request = HTTP.post(url, headers, json_step4)
+        println("✔")
+        return Job(reg, qobj, jobid)
+    else 
+        println("❌")
+    end
 end
 
-function status()
+function status(job::Job)
+    jobid = job.jobid
+    id = job.reg.id
     url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)?access_token=$(id)"
     result = HTTP.get(url)
     response_json = String(result.body)
@@ -90,24 +144,42 @@ function status()
 end
 
 # get result
-url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)/resultDownloadUrl?access_token=$(id)"
-result = HTTP.get(url)
-response_json = String(result.body)
-response_parsed = JSON.parse(response_json)
-downloadUrl = response_parsed["url"]
-final_res = HTTP.get(downloadUrl)
-response_json = String(final_res.body)
-response_parsed = JSON.parse(response_json)
-res = response_parsed["results"]
+function getresult(job::Job)
+    jobid = job.jobid
+    id = job.reg.id
+    url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)/resultDownloadUrl?access_token=$(id)"
+    print("Connecting to $(job.reg.device)...")
+    result = HTTP.get(url)
+    if result.status == 200
+        println("✔")
+    else
+        println("❌")
+    end
+    response_json = String(result.body)
+    response_parsed = JSON.parse(response_json)
+    downloadUrl = response_parsed["url"]
+    final_res = HTTP.get(downloadUrl)
+    
+    if final_res.status == 200
+        print("Fetching result from $(job.reg.device)...")
+        # STEP8: Confirm the data was downloaded
+        url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)/resultDownloaded?access_token=$(id)"
+        json_step8 = """{
+            "data": "none",
+            "json": "none"
+            }"""
+        result = HTTP.post(url, headers, json_step8)
+        if result.status == 200
+            println("✔")
+        else
+            println("❌")
+        end
+        response_json = String(final_res.body)
+        response_parsed = JSON.parse(response_json)
+        res = response_parsed["results"]
+    end
+end
 
-
-# STEP8: Confirm the data was downloaded
-url = "https://api.quantum-computing.ibm.com/api/Network/ibm-q/Groups/open/Projects/main/Jobs/$(jobid)/resultDownloaded?access_token=$(id)"
-json_step8 = """{
-    "data": "none",
-    "json": "none"
-}"""
-result = HTTP.post(url, headers, json_step8)
 
 
 
